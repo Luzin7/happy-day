61478aec78f3cf61d1c43a39d84fff3f7aa2d95e
Luzin7 <lvictor.dutra@gmail.com>
2022-08-13 18:50:04 -0300

    feat: Add responsive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import TributeText from "./components/TributeText";
 
+import './styles/responsive.css'
+
 function App() {
   return (
     <div className="App">
